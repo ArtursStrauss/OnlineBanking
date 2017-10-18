98ac97d053970d486f4de4f47b89b8c210358051
artustra <arturs.strauss@gmail.com>
2017-10-18 18:23:25 +0300

    Initial commit

---
+++ src/main/java/lv/javaguru/java3/OnlineBanking/core/domain/Client.java
@@ -0,0 +1,53 @@
+package lv.javaguru.java3.OnlineBanking.core.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(generator = "clients_seq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "clients_seq", sequenceName = "clients_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "login", nullable = false)
+    private String login;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
